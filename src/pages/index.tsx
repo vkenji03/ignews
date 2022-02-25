0f28756ed5a63bed9b6751c6375caa4b58ff259b
Vitor Kenji <viitor.kenji@gmail.com>
2022-02-25 18:04:34 -0300

    initial configuration and installing typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+// cada arquivo dentro de pages o next cria uma rota para ele
+// so pode usar export default quando utiliza o next
+
+export default function Home() {
+  return (
+    <h1>Hello World</h1>
+  )
+}
