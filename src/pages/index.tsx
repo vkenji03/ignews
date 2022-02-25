afaf1a2620d4ca234e9c7438d6326cc787022b43
Vitor Kenji <viitor.kenji@gmail.com>
2022-02-25 19:00:39 -0300

    code after class estilizacao com sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 // cada arquivo dentro de pages o next cria uma rota para ele
 // so pode usar export default quando utiliza o next
 
+// utilizar o CSS modules do next
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    // utilizar o CSS modules do next
+    <h1 className={styles.title}>
+      Hello <span>World</span>
+    </h1>
   )
 }
