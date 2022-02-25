f6dd9aa3ac034337fc331e4a81ebff9afb1ef42a
Vitor Kenji <viitor.kenji@gmail.com>
2022-02-25 20:51:21 -0300

    after class title dinamico por pagina

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,11 +10,7 @@ export default class MyDocument extends Document {
     return(
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
-
-          <title>ig.news</title>
         </Head>
         <body>
           {/* todo conteudo da aplicacao eh renderizado dentro de Main */}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
 // cada arquivo dentro de pages o next cria uma rota para ele
 // so pode usar export default quando utiliza o next
 
+import Head from 'next/head';
+
 // utilizar o CSS modules do next
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    // utilizar o CSS modules do next
-    <h1 className={styles.title}>
-      Hello <span>World</span>
-    </h1>
+    <>
+      {/*
+        Tudo o que coloca dentro do componente Head eh adicionado dentro da tag head no HTML que eh renderizado para o usuario
+        Precisa fazer isso pois _document so eh renderizado uma vez, entao caso o title esteja dentro desse arquivo todas as paginas
+        terao o mesmo title
+      */}
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      {/* utilizar o CSS modules do next */}
+      <h1 className={styles.title}>
+        Hello <span>World</span>
+      </h1>
+    </>
   )
 }
