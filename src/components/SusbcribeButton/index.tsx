c33ad08f27da2517131fc5b8a82dc3b26ff75449
Vitor Kenji <viitor.kenji@gmail.com>
2022-03-09 13:56:39 -0300

    after class gerando sessao de checkout

--- src/components/SusbcribeButton/index.tsx
+++ src/components/SusbcribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, useSession } from 'next-auth/react';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,10 +6,26 @@ interface SubscribeButtonProps {
 };
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  // checkout session eh basicamente uma URL que o usuario eh direcionado para preencher as informacoes de pagamento e apos terminar
+  // ele eh redirecionado para outra pagina
+
+  const { data: session } = useSession();
+
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from 'next-auth/react';
+import { stripe } from '../../services/stripe';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    // cookies podem ser acessados tanto pelo frontend como pelo backend, desde que ambos estejam no mesmo dominio, diferente do
+    // localstorage que so eh acessivel pelo browser
+    
+    const session = await getSession({ req });
+
+    const stripeCustomer = await stripe.customers.create({
+      email: session.user.email
+      // metadata
+    });
+
+    const stripeCheckoutSession = stripe.checkout.sessions.create({
+      customer: stripeCustomer.id,
+      payment_method_types: ['card'],
+      billing_address_collection: "required",
+      line_items: [
+        { price: 'price_1KZ3swJgPt2vNe7OJfOJwRJw', quantity: 1 }
+      ],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL
+    });
+
+    return res.status(200).json({ sessionId: stripeCheckoutSession });
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method not allowed');
+  }
+}
