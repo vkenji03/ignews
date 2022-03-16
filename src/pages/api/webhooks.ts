066e37b4eeb1a6931626ccbc6ca264c117078ed5
Vitor Kenji <viitor.kenji@gmail.com>
2022-03-16 16:32:24 -0300

    after class ouvindo eventos do stripe

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,7 +1,58 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { Readable } from 'stream';
+import Stripe from 'stripe';
+import { stripe } from '../../services/stripe';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('evento recebido');
+// codigo para a integracao entre o stripe e o next, a requisicao do webhook do stripe nao chega completa, por isso precisamos 
+// esperar todos os chunks chegarem para podermos executar o codigo
+async function buffer(readable: Readable) {
+  const chunks = [];
 
-  res.status(200).json({ ok: true });
+  for await (const chunk of readable) {
+    chunks.push(
+      typeof chunk === 'string' ? Buffer.from(chunk) : chunk
+    );
+  }
+
+  return Buffer.concat(chunks);
+}
+
+// sem isso o next espera uma request em formato JSON
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
+const relevantEvents = new Set([
+  'checkout.session.completed'
+]);
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const buf = await buffer(req);
+  
+    // sempre q usa webhooks precisa garantir que quem esta tentando acessar a rota seja uma pessoa que pode fazer isso
+    // por isso precisa de uma "senha", ja que essa rota eh uma rota de backend normal
+    const secret = req.headers['stripe-signature'];
+
+    // fazer a validacao de quem esta tentando acessar a rota, forma recomendada pelo stripe
+    let event: Stripe.Event;
+    try {
+      event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
+    } catch (err) {
+      return res.status(400).send(`Webhook error: ${err.message}`);
+    }
+
+    const { type } = event;
+
+    if (relevantEvents.has(type)) {
+      console.log('Evento recebido', event);
+    } 
+
+    res.json({ received: true });
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method nor allowed');
+  }
 }
